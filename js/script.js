c8c0fb431f7bf7f105052cb9cf313ef266ce9462
santi <santimejiaok@gmail.com>
2024-08-02 01:52:36 -0500

    minor changes

--- js/script.js
+++ js/script.js
@@ -1,14 +1,39 @@
-
 header = document.querySelector('header')
 buttonBack = document.querySelector('.buttonBack')
+iconProducts = document.querySelectorAll('.iconProducts')
+front = document.querySelectorAll('.front')
+back = document.querySelectorAll('.back')
 
 
-window.addEventListener('scroll', function() {
-    if (window.scrollY > 10) {
-        header.classList.add('action')
-        buttonBack.classList.add('action')
-    } else {
-        header.classList.remove('action')
-        buttonBack.classList.remove('action')
-    }
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY > 10) {
+    header.classList.add('action')
+    buttonBack.classList.add('action')
+  } else {
+    header.classList.remove('action')
+    buttonBack.classList.remove('action')
+  }
 })
+
+iconProducts.forEach(button => {
+    button.addEventListener('click', (e) => {
+        const buttonId = e.target.id
+        front.addEventListener('click', () => {
+            front.classList.add('active')
+            back.classList.add('active')
+        }  )
+        console.log(buttonId)
+    })
+    }
+)
+
+ searchInput = document.querySelector('#search-btn')
+ searchCuadro = document.querySelector('#searchInput')
+searchInput.addEventListener('click', () => {
+    searchCuadro.classList.add('inputSearchActive')
+    }
+)
+
+
+
