773c923dd9d180f66a8a798fd96022b898087a0d
santi <santimejiaok@gmail.com>
2024-08-11 02:14:12 -0500

    adding js to products

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
 header = document.querySelector('header')
 buttonBack = document.querySelector('.buttonBack')
-iconProducts = document.querySelectorAll('.iconProducts')
 front = document.querySelectorAll('.front')
 back = document.querySelectorAll('.back')
+containerProducts = document.querySelector('.grid-container')
 
+let products = []
+
+fetch('./js/products.json')
+  .then(response => response.json())
+  .then(data => {
+    products = data
+    loadProducts(products)
+  })
 
 
 window.addEventListener('scroll', function () {
@@ -16,17 +24,37 @@ window.addEventListener('scroll', function () {
   }
 })
 
-iconProducts.forEach(button => {
-    button.addEventListener('click', (e) => {
-        const buttonId = e.target.id
-        front.addEventListener('click', () => {
-            front.classList.add('active')
-            back.classList.add('active')
-        }  )
-        console.log(buttonId)
-    })
-    }
-)
+function loadProducts(products) {
+  containerProducts.innerHTML = ''
+
+  products.forEach(element => {
+      const li = document.createElement('li')
+      li.innerHTML = `
+      <div class="imgs-best-sellers">
+                    <div class="img-container">
+                    <img src="${element.imgs.front}" class="front alt${element.name}">
+                    <img src="${element.imgs.back}" class="back alt${element.name}">
+                    <button id="${element.id}" class="iconProducts">
+                       <ion-icon name="bag-add-outline" aria-hidden="true" role="img" class="md hydrated"></ion-icon>
+                    </button>
+                    </div>
+                    <div class="calificacion-estrellas">
+                        <ion-icon name="star" ></ion-icon>
+                        <ion-icon name="star" ></ion-icon>
+                        <ion-icon name="star" ></ion-icon>
+                        <ion-icon name="star" ></ion-icon>
+                        <ion-icon name="star" ></ion-icon>
+                        <span class="span">(1)</span>
+                    </div>
+                    
+                    <a href="#">${element.name}</a>
+                    <data>$${element.price}</data>
+                    
+                  </div>
+      `;
+      containerProducts.appendChild(li)
+  });
+}
 
  searchInput = document.querySelector('#search-btn')
  searchCuadro = document.querySelector('#searchInput')
@@ -34,6 +62,3 @@ searchInput.addEventListener('click', () => {
     searchCuadro.classList.add('inputSearchActive')
     }
 )
-
-
-
